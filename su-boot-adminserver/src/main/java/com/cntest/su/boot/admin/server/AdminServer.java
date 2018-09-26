bce0ce3a05d68e7ce4834383da5ff56485c2d309
wanglei <wangl@seaskylight.com>
2018-09-26 18:31:19 +0800

    20180926 adminServer

---
+++ su-boot-adminserver/src/main/java/com/cntest/su/boot/admin/server/AdminServer.java
@@ -0,0 +1,24 @@
+package com.cntest.su.boot.admin.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Spring
+ *
+ * @author LYC
+ * @version 1.0 Create By 2018年3月21日
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AdminServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServer.class, args);
+    }
+};
